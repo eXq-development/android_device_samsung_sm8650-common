ccd4eb7d435833053fcd3a954183d7b6549bc405
chandu078 <chandudyavanapelli03@gmail.com>
2024-12-21 16:24:35 -0300

    sm8650-common: Update config.fs to LA.VENDOR.14.3.0.r1-11500-lanai.0
    Change-Id: Ic5fa89aaf68ad330826d7dc37445788d80d406e2
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- configs/config.fs
+++ configs/config.fs
@@ -37,6 +37,15 @@ value:2912
 [AID_VENDOR_THALES_STRONGBOX]
 value:2913
 
+[AID_VENDOR_QCC]
+value:2914
+
+[AID_VENDOR_NXP_AUTHSECRET]
+value:2915
+
+[AID_VENDOR_THALES_WEAVER]
+value:2916
+
 [AID_VENDOR_THALES_WEAVER]
 value:2914
 
@@ -52,15 +61,9 @@ user: AID_VENDOR_THALES_WEAVER
 group: AID_SYSTEM
 caps: SYS_ADMIN SYS_NICE
 
-[vendor/bin/hw/android.hardware.security.keymint-service.strongbox]
+[vendor/bin/hw/android.hardware.weaver-service.thales]
 mode: 0755
-user: AID_VENDOR_NXP_STRONGBOX
-group: AID_SYSTEM
-caps: SYS_ADMIN SYS_NICE
-
-[vendor/bin/hw/android.hardware.weaver@1.0-service]
-mode: 0755
-user: AID_VENDOR_NXP_WEAVER
+user: AID_VENDOR_THALES_WEAVER
 group: AID_SYSTEM
 caps: SYS_ADMIN SYS_NICE
 
